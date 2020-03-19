9225b782509fb9c589b5a2a1dbf29c8997b757e2
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-19 03:43:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ require 'directors_database'
 def gross_for_director(director_data)
 
   gross = 0;
-  gross_length = nds[:name][:movies].length
+  gross_length = director_data[:name][:movies].length
   counter = 0;
 
   while counter < gross_length do

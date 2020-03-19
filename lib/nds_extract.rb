969139771c70390e7e3ef5aecae167e19e07b5bd
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-19 03:35:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,6 +5,16 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
 
+  gross = 0;
+  gross_length = nds[:name][:movies][:worldwide_grosses].length
+  counter = 0;
+
+  while counter < gross_length do
+    gross += nds[:name][:movies]
+  end
+
+  end
+
 end
 
 # Write a method that, given an NDS creates a new Hash
@@ -12,6 +22,27 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
-  result = {}
-  nil
+  directors_total = {}
+
+  #directors_total
 end
+
+
+[{:name=>"Stephen Spielberg",
+  :movies=>[{:title=>"Jaws",:studio=>"Universal",:worldwide_gross=>260000000,:release_year=>1975},{:title=>"Close Encounters of the Third Kind",:studio=>"Columbia",:worldwide_gross=>135189114,:release_year=>1977},
+    {:title=>"Raiders of the Lost Ark",
+     :studio=>"Paramount",
+     :worldwide_gross=>248159971,
+     :release_year=>1981},
+    {:title=>"E.T. the Extra-terrestrial",
+     :studio=>"Universal",
+     :worldwide_gross=>435110554,
+     :release_year=>1982},
+    {:title=>"Schindler's List",
+     :studio=>"Universal",
+     :worldwide_gross=>96898818,
+     :release_year=>1993},
+    {:title=>"Lincoln",
+     :studio=>"Buena Vista",
+     :worldwide_gross=>182207973,
+     :release_year=>2012}]},
